e29f298a54182073778ecc0a6c207b0b7054e230
vanderpit <59393315+vanderpit@users.noreply.github.com>
2021-02-28 09:42:52 +0100

    native support

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './components/HomeScreen'
+import SplashScreen from 'react-native-splash-screen'
 
 const Stack = createStackNavigator()
 
 export default function App() {
+  React.useEffect(()=>{
+    SplashScreen.hide()
+  },[])
   return (
     <NavigationContainer>
       <Stack.Navigator>
